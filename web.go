394c35b6477ca0b6f99e7f53b9174ae349510df2
Mark Johnson <markjohnson@gmail.com>
2015-05-11 21:38:21 -0700

    Switch to using Gorilla Mux for routing.

--- web.go
+++ web.go
@@ -4,11 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", hello)
+	r := mux.NewRouter()
+	r.HandleFunc("/", hello)
+
 	fmt.Println("listening...")
+
+	// Set up go http to pass off all web access to gorilla mux
+	http.Handle("/", r)
 	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 	if err != nil {
 		panic(err)
