78214c42ec60128a8303096ec8e7f3341aea7dd4
Bernhard Poess <bernhardpoess@gmail.com>
2015-05-12 21:53:54 -0700

    Initial commit

--- web.go
+++ web.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/googollee/go-socket.io"
 	"github.com/gorilla/mux"
 )
 
@@ -14,6 +15,24 @@ type Board struct {
 }
 
 func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
 	// Use Gorilla routing for all real routes
 	r := mux.NewRouter()
 	r.HandleFunc("/", indexHandler)
